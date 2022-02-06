f1dc389fb0789e4ece507447618bda3c95aae946
Pranav Chandane <pranavhchandane@gmail.com>
2022-02-06 13:51:53 +0530

    Done

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const rateLimiter = require("express-rate-limit");
 // Swagger
 const SwaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
-const swaggerDocument = YAML.load("./swagger.yaml");
+const loadSwaggerUI = YAML.load("./swagger.yaml");
 
 app.set("trust proxy", 1);
 app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100 }));
@@ -31,7 +31,8 @@ app.use(cors());
 app.use(xss());
 
 app.get("/", (req, res) => res.send('<h1>Jobs API</h1><a href="/api-docs">API Documentation</a>'));
-app.get("/api-docs", SwaggerUI.serve, SwaggerUI.setup(swaggerDocument));
+app.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(loadSwaggerUI));
+
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
